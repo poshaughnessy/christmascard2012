06887b1c7c064712cdadf4f0f2419868013a3432
Peter O'Shaughnessy <p_os@yahoo.co.uk>
2012-12-18 21:08:45 +0000

    First commit

---
+++ js/main.js
@@ -0,0 +1,161 @@
+var camera;
+var cssScene, cssRenderer;
+var canvasScene, canvasRenderer;
+
+var cardFront;
+
+var openingCard = false;
+
+var particle;
+var particles = [];
+var particleImage = new Image();
+particleImage.src = 'images/ParticleSmoke.png';
+
+
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 0, 0, 400 );
+
+    cssScene = new THREE.Scene();
+
+    var cardFrontEl = document.createElement( 'div' );
+    cardFrontEl.style.width = '300px';
+    cardFrontEl.style.height = '450px';
+    cardFrontEl.style.background = new THREE.Color( 0xeeeeee ).getContextStyle();
+
+    cardFront = new THREE.CSS3DObject( cardFrontEl );
+    cardFront.position.x = 0;
+    cardFront.position.y = 0;
+    cardFront.position.z = 0;
+    cssScene.add( cardFront );
+
+    //
+
+    cssRenderer = new THREE.CSS3DRenderer();
+    cssRenderer.setSize( window.innerWidth, window.innerHeight );
+    cssRenderer.domElement.style.position = 'absolute';
+    cssRenderer.domElement.style.top = 0;
+    document.body.appendChild( cssRenderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+
+    document.addEventListener('keydown', onKeyDown, false);
+
+    initSnow();
+
+}
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    if( openingCard ) {
+
+        if( cardFront.rotation.y < (45 / 180 * Math.PI) ) {
+
+            // TODO tween...
+
+        }
+
+    }
+
+    animateSnow();
+
+    cssRenderer.render( cssScene, camera );
+    canvasRenderer.render( canvasScene, camera );
+
+}
+
+function openCard() {
+
+    openingCard = true;
+
+}
+
+/* Thanks to the JavaScript PixelPounding demos from github.com/sebleedelisle */
+function initSnow() {
+
+    var container = document.createElement('div');
+
+    container.style.position = 'absolute';
+    container.style.top = '0';
+    container.style.left = '0';
+
+    document.body.appendChild(container);
+
+    canvasScene = new THREE.Scene();
+
+    canvasRenderer = new THREE.CanvasRenderer();
+    canvasRenderer.setSize(window.innerWidth, window.innerHeight);
+    var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture(particleImage) } );
+
+    for (var i = 0; i < 100; i++) {
+
+        particle = new Particle3D(material);
+        particle.position.x = Math.random() * 2000 - 1000;
+        particle.position.y = Math.random() * 2000 - 1000;
+        particle.position.z = Math.random() * 2000 - 1000;
+        particle.scale.x = particle.scale.y =  1;
+        canvasScene.add( particle );
+
+        particles.push(particle);
+    }
+
+    canvasRenderer.domElement.style.position = 'absolute';
+
+    container.appendChild( canvasRenderer.domElement );
+
+}
+
+function animateSnow() {
+
+    for(var i = 0; i < particles.length; i++)  {
+
+        var particle = particles[i];
+        particle.updatePhysics();
+
+        if(particle.position.y < -1000) {
+            particle.position.y += 2000;
+        }
+        if(particle.position.x > 1000) {
+            particle.position.x -= 2000;
+        } else if(particle.position.x < -1000) {
+            particle.position.x += 2000;
+        }
+        if(particle.position.z > 1000) {
+            particle.position.z -= 2000;
+        } else if(particle.position.z<-1000) {
+            particle.position.z+=2000;
+        }
+
+    }
+
+}
+
+function onWindowResize() {
+
+    cssRenderer.setSize( window.innerWidth, window.innerHeight );
+    canvasRenderer.setSize( window.innerWidth, window.innerHeight );
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+}
+
+function onKeyDown(event) {
+
+    if (/^(input|textarea)$/i.test(event.target.nodeName)) return;
+
+    switch (event.keyCode) {
+        case 39: // right arrow
+        case 40: // down arrow
+        case 32: // space
+            openCard();
+            break;
+    }
+
+}
